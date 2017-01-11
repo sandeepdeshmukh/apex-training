35b37b9622229c282576131810cccfc2c112aa72
Chinmay Kolhatkar <chinmay@datatorrent.com>
2017-01-11 14:40:12 +0530

    Added parser and formatter

--- kafka-to-hdfs-sync/src/main/java/com/datatorrent/apps/Application.java
+++ kafka-to-hdfs-sync/src/main/java/com/datatorrent/apps/Application.java
@@ -19,21 +19,14 @@
 
 package com.datatorrent.apps;
 
-import java.util.Map;
-
-import org.apache.apex.malhar.kafka.KafkaSinglePortInputOperator;
-import org.apache.apex.malhar.lib.fs.GenericFileOutputOperator.BytesFileOutputOperator;
-import org.apache.apex.malhar.lib.fs.GenericFileOutputOperator.StringFileOutputOperator;
-import org.apache.hadoop.conf.Configuration;
-
-import com.google.common.collect.Maps;
-
 import com.datatorrent.api.DAG;
 import com.datatorrent.api.StreamingApplication;
 import com.datatorrent.api.annotation.ApplicationAnnotation;
 import com.datatorrent.contrib.formatter.CsvFormatter;
 import com.datatorrent.contrib.parser.CsvParser;
-import com.datatorrent.lib.transform.TransformOperator;
+import org.apache.apex.malhar.kafka.KafkaSinglePortInputOperator;
+import org.apache.apex.malhar.lib.fs.GenericFileOutputOperator.StringFileOutputOperator;
+import org.apache.hadoop.conf.Configuration;
 
 @ApplicationAnnotation(name = "Kafka-to-HDFS-Sync")
 public class Application implements StreamingApplication
@@ -42,57 +35,15 @@ public class Application implements StreamingApplication
   public void populateDAG(DAG dag, Configuration conf)
   {
     KafkaSinglePortInputOperator kafkaInputOperator = dag.addOperator("kafkaInput", KafkaSinglePortInputOperator.class);
-    BytesFileOutputOperator fileOutput = dag.addOperator("fileOutput", BytesFileOutputOperator.class);
 
-    dag.addStream("data", kafkaInputOperator.outputPort, fileOutput.input);
+    CsvParser parser = dag.addOperator("csvParser", CsvParser.class);
+    CsvFormatter formatter = dag.addOperator("csvFormatter", CsvFormatter.class);
 
-    /*
-     * To add custom logic to your DAG, add your custom operator here with
-     * dag.addOperator api call and connect it in the dag using the dag.addStream
-     * api call. 
-     * 
-     * For example: 
-     * 
-     * To parse incoming csv lines, transform them and outputting them on kafka.
-     * kafkaInput->CSVParser->Transform->CSVFormatter->fileOutput can be achieved as follows
-     * 
-     * Adding operators: 
-     * CsvParser csvParser = dag.addOperator("csvParser", CsvParser.class);
-     *
-     * TransformOperator transform = dag.addOperator("transform", new TransformOperator());
-     * Map<String, String> expMap = Maps.newHashMap();
-     * expMap.put("name", "{$.name}.toUpperCase()");
-     * transform.setExpressionMap(expMap);
-     * CsvFormatter formatter = dag.addOperator("formatter", new CsvFormatter());
-     * 
-     * Use StringFileOutputOperator instead of BytesFileOutputOperator i.e. 
-     * Replace the following line below:
-     * BytesFileOutputOperator fileOutput = dag.addOperator("fileOutput", BytesFileOutputOperator.class);
-     * with this lines:
-     * StringFileOutputOperator fileOutput = dag.addOperator("fileOutput", new StringFileOutputOperator());
-     *
-     * Connect these operators with approriate streams
-     * Replace the following line below:
-     * dag.addStream("data", kafkaInputOperator.outputPort, fileOutput.input);
-     * 
-     * with these lines:
-     * dag.addStream("data", kafkaInputOperator.outputPort, csvParser.in);
-     * dag.addStream("pojo", csvParser.out, transform.input);
-     * dag.addStream("transformed", transform.output, formatter.in);
-     * dag.addStream("string", formatter.out, fileOutput.input);
-     * 
-     * In ApplicationTests.java->
-     * Replace the following line from setup()
-     * outputFilePath = outputDir + "/output.txt_2.0";
-     * with
-     * outputFilePath = outputDir + "/output.txt_5.0";
-     * 
-     * Replace the following line from compare()
-     * Assert.assertArrayEquals(lines, output.split("\\n"));
-     * with
-     * Assert.assertArrayEquals(lines_transformed, output.split("\\n"));
-     * 
-     */
+    StringFileOutputOperator fileOutput = dag.addOperator("fileOutput", StringFileOutputOperator.class);
+
+    dag.addStream("toParser", kafkaInputOperator.outputPort, parser.in);
+    dag.addStream("toFormatter", parser.out, formatter.in);
+    dag.addStream("toHDFS", formatter.out, fileOutput.input);
   }
 
 }
